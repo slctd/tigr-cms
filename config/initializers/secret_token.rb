30be74046499d33c3a4c68571d02e6c705f92022
Artur Trofimov <arturtr@gmail.com>
2013-06-10 02:59:01 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TigrCms::Application.config.secret_token = '14a5249d7445b463cee07633b3b46adf713751b1fbe0527f33f0fdf4863d99165a97076e9343e1bf0a0920de196206193333568f65c758f98d0ef4464195a625'
